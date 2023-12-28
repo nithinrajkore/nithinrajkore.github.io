7a026ed4dff6d2dd339d07ba6232cd2b5c90563b
nithinrajkore <154859185+nithinrajkore@users.noreply.github.com>
2023-12-28 05:19:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",{
+    startDelay:1000,
+    strings: ["Data Scientist!"],
+    typeSpeed:100,
+    backSpeed:50,
+    // backDelay:1000,
+    loop:true,
+    // hideCursor:true
+});
